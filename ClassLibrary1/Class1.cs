5bfb9146c780631ec4cfa25c25d62cd613238ca1
Michel Ferland <lifef0rm@hotmail.com>
2020-06-15 19:26:37 -0400

    a

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -8,5 +8,10 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        void DoSomething()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("allo");
+        }
     }
 }
